c833151c992f0168d5734439f1ed3605d23d3bed
Viraj Changani <145748780+virajchangani@users.noreply.github.com>
2024-12-25 16:24:00 +0530

    Update index.js

--- Admin Panel_Project/index.js
+++ Admin Panel_Project/index.js
@@ -11,7 +11,7 @@ app.use(cookie())
 app.use("/",require("./routes/rout"));
 
 app.use("/",express.static(path.join(__dirname,"public_1")));
-app.use(express.static(path.join(__dirname,"public")));
+
 
 
 
